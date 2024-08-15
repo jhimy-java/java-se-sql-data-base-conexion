ca68afb983d39c97ace9d9819897a528d971913d
jhimydevg <chance.jhimy@gmail.com>
2024-08-15 15:53:24 -0400

    leer y crear datos con JDBC

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,23 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) {
         Connection myCon = null;
-        Statement myStmt = null;
-        ResultSet rs = null;
+        PreparedStatement myStmt = null;
+        //ResultSet rs = null;
 
         try{
             myCon= DriverManager.getConnection("jdbc:mysql://localhost:3306/project","root","jhimy");
             System.out.println("Conexion establecida");
 
-            myStmt = myCon.createStatement();
-            rs = myStmt.executeQuery("SELECT * FROM employees");
-            while(rs.next()){
-                System.out.println(rs.getString("first_name"));
+            String sql=("INSERT INTO employees (first_name, pa_surname) VALUES (?, ?)");
+            myStmt = myCon.prepareStatement(sql);
+
+            myStmt.setString(1,"Johana");
+            myStmt.setString(2,"Smith");
+
+            int rowsAffected = myStmt.executeUpdate();
+
+            if(rowsAffected>0){
+                System.out.println("Se ha creado un nuevo cliente");
             }
         } catch (Exception e) {
             e.printStackTrace();
